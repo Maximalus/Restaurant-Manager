0900d5742d4aaf376dfc7930ad7bc9b6517747df
Maksym Matlo <maximalys@gmail.com>
2021-02-13 11:35:40 +0200

    Changed JpaRepository to CrudRepository in all repositories

--- src/main/java/com/maximalus/repository/IngredientGroupRepository.java
+++ src/main/java/com/maximalus/repository/IngredientGroupRepository.java
@@ -1,9 +1,9 @@
 package com.maximalus.repository;
 
 import com.maximalus.model.IngredientGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IngredientGroupRepository extends JpaRepository<IngredientGroup, Long> {
+public interface IngredientGroupRepository extends CrudRepository<IngredientGroup, Long> {
 }
--- src/main/java/com/maximalus/repository/ReportRepository.java
+++ src/main/java/com/maximalus/repository/ReportRepository.java
@@ -1,9 +1,9 @@
 package com.maximalus.repository;
 
 import com.maximalus.model.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Long> {
+public interface ReportRepository extends CrudRepository<Report, Long> {
 }
