d97fbd36d5250e29333092bc3b3dccf12cab4271
Maksym Matlo <maximalys@gmail.com>
2021-03-06 22:18:17 +0200

    Log config fixed

--- src/main/java/com/maximalus/dto/IngredientDto.java
+++ src/main/java/com/maximalus/dto/IngredientDto.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
-@Slf4j
 @Data
 public class IngredientDto {
     private Long id;
--- src/main/java/com/maximalus/dto/UserDto.java
+++ src/main/java/com/maximalus/dto/UserDto.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
-@Slf4j
 @Data
 public class UserDto {
     private Long id;
