a7246c88799455158e42cabda6f9c0cebc29add2
Maksym Matlo <maximalys@gmail.com>
2021-03-12 15:57:20 +0200

    Minor changes

--- src/main/java/com/maximalus/Application.java
+++ src/main/java/com/maximalus/Application.java
@@ -1,11 +1,11 @@
 package com.maximalus;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(com.maximalus.Application.class, args);
 	}
 }
