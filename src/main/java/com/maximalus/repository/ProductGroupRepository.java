faba5d2f514650435091a2a6aac8f0e7f519de6b
Maksym Matlo <maximalys@gmail.com>
2021-03-14 13:28:43 +0200

    Deleted classes accordingly to new structure of model classes

--- src/main/java/com/maximalus/repository/ProductGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maximalus.repository;
-
-import com.maximalus.model.ProductGroup;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductGroupRepository extends CrudRepository<ProductGroup, Long> {
-}
