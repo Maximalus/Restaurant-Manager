5c9e7c6ffdaf4de4e14bcc0432e8e477c787bca7
Maksym Matlo <maximalys@gmail.com>
2021-04-21 14:45:17 +0300

    Changed IngredientGroupController

--- src/main/java/com/maximalus/controler/IngredientGroupController.java
+++ src/main/java/com/maximalus/controler/IngredientGroupController.java
@@ -13,7 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,9 +48,9 @@ public class IngredientGroupController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public IngredientGroup createIngredientGroup(String name){
+    public IngredientGroup createIngredientGroup(@RequestBody String groupName){
         IngredientGroup ingredientGroup = new IngredientGroup();
-        ingredientGroup.setName(name);
+        ingredientGroup.setName(groupName);
         ingredientGroup.setCreationDate(LocalDateTime.now());
         ingredientGroup.setChangingDate(LocalDateTime.now());
         return ingredientGroupService.save(ingredientGroup);
@@ -55,9 +59,9 @@ public class IngredientGroupController {
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public IngredientGroup updateIngredientGroup(@PathVariable Long id,
-                                       String name){
+                                                 @RequestBody String groupName){
         IngredientGroup ingredientGroup = ingredientGroupService.findById(id);
-        ingredientGroup.setName(name);
+        ingredientGroup.setName(groupName);
         ingredientGroup.setChangingDate(LocalDateTime.now());
         return ingredientGroupService.update(ingredientGroup);
     }
