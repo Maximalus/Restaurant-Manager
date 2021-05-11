fbe7e0cc0a4f9ee486d962fe13aa199200c6408a
Maksym Matlo <maximalys@gmail.com>
2021-05-11 17:04:57 +0300

    Added Spring Security with JWT

---
+++ src/main/java/com/maximalus/config/SpringSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.maximalus.config;
+
+
+import com.maximalus.security.JwtCsrfFilter;
+import com.maximalus.security.JwtTokenRepository;
+import com.maximalus.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    private JwtTokenRepository jwtTokenRepository;
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Bean
+    public PasswordEncoder devPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+                .addFilterAt(new JwtCsrfFilter(jwtTokenRepository, resolver), CsrfFilter.class)
+                .csrf().ignoringAntMatchers("/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/login")
+                .authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(((request, response, e) -> resolver.resolveException(request, response, null, e)));
+    }
+}
